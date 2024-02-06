512463102d10d88400260acebbf9ee9666f7e39c
Tainna <tainnaandryelli@gmail.com>
2024-02-06 10:04:24 -0300

    ✨feat: Tab Navegation

---
+++ script.js
@@ -0,0 +1,25 @@
+// Tab Navegation
+function initTabNav() {
+  const tabMenu = document.querySelectorAll(".js-tabmenu li");
+  const tabContent = document.querySelectorAll(".js-tabcontent section");
+
+  if (tabMenu.length && tabContent.length) {
+    tabContent[0].classList.add("active");
+
+    function activeTab(index) {
+      tabContent.forEach((item) => {
+        item.classList.remove("active");
+      });
+
+      tabContent[index].classList.add("active");
+    }
+
+    tabMenu.forEach((item, index) => {
+      item.addEventListener("click", () => {
+        activeTab(index);
+      });
+    });
+  }
+}
+
+initTabNav();
